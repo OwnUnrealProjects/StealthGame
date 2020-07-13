9677106f7c416fa0173a8bd05e74ea15fa4bab0f
Leonsius <lfirtskhalava@gmail.com>
2020-07-14 01:17:22 +0400

    04FPS DebugTool

---
+++ Source/FPSGame/DebugTool/DebugLog.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+//#include "CoreMinimal.h"
+
+#define DEBUGING 1
+#define DEBUGING_ENEBALE(x) DEBUGING x
+
+#if DEBUGING
+
+#define  LOG_S(x) UE_LOG(LogTemp, Warning, TEXT("[ Function -- %s ]  String = %s"), *FString(__FUNCTION__), *x)
+#define  LOG_I(x) UE_LOG(LogTemp, Warning, TEXT("[ Funtion  -- %s ]  Integer = %i"),*FString(__FUNCTION__), x)
+
+#else
+
+#define  LOG_S(x)
+#define  LOG_I(x)
+
+#endif
